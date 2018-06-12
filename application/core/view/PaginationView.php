8b18b9ad79a288ba6c31f40b9beb9a43b693c24d
max7722 <max.milash@yandex.ru>
2018-06-12 15:10:10 +0300

    Испарвил пагинатор

--- application/core/view/PaginationView.php
+++ application/core/view/PaginationView.php
@@ -65,34 +65,36 @@ class PaginationView extends View
     protected function initRender()
     {
         $iCount = $this->aParams['count'];
-        if ($iCount > $this->aParams['maxCount']) {
-            $this->aParams['isComposite'] = 1;
+        if ($iCount > 1) {
+            if ($iCount > $this->aParams['maxCount']) {
+                $this->aParams['isComposite'] = 1;
 
-            $iHalfLeftPagin = intval($this->aParams['leftMaxCount'] / 2) + 1;
-            $iRightCount = $this->aParams['maxCount'] - $this->aParams['leftMaxCount'];
-            if ($this->aParams['active'] <= $iHalfLeftPagin) {
-                $this->aParams['leftPagin'] = range(1, $this->aParams['leftMaxCount']);
-                $this->aParams['rightPagin'] = range($iCount - $iRightCount, $iCount);
-            } elseif ($iCount - $this->aParams['active'] <= $iRightCount) {//если елемент находится в правой части пагинатора
-                $this->aParams['leftPagin'] = range(1, $iRightCount);
-                $this->aParams['rightPagin'] = range($iCount - $this->aParams['leftMaxCount'], $iCount);
+                $iHalfLeftPagin = intval($this->aParams['leftMaxCount'] / 2) + 1;
+                $iRightCount = $this->aParams['maxCount'] - $this->aParams['leftMaxCount'];
+                if ($this->aParams['active'] <= $iHalfLeftPagin) {
+                    $this->aParams['leftPagin'] = range(1, $this->aParams['leftMaxCount']);
+                    $this->aParams['rightPagin'] = range($iCount - $iRightCount, $iCount);
+                } elseif ($iCount - $this->aParams['active'] <= $iRightCount) {//если елемент находится в правой части пагинатора
+                    $this->aParams['leftPagin'] = range(1, $iRightCount);
+                    $this->aParams['rightPagin'] = range($iCount - $this->aParams['leftMaxCount'], $iCount);
+                } else {
+                    $this->aParams['leftPagin'] = range(1, 2);
+                    $this->aParams['rightPagin'] = range($iCount - $iRightCount, $iCount);
+                    $this->aParams['midlePagin'] = range($this->aParams['active'] - 1, $this->aParams['active'] + 1);
+                }
             } else {
-                $this->aParams['leftPagin'] = range(1, 2);
-                $this->aParams['rightPagin'] = range($iCount - $iRightCount, $iCount);
-                $this->aParams['midlePagin'] = range($this->aParams['active'] - 1, $this->aParams['active'] + 1);
+                $this->aParams['isComposite'] = 0;
+
+                $this->aParams['leftPagin'] = range(1, $iCount);
             }
-        } else {
-            $this->aParams['isComposite'] = 0;
 
-            $this->aParams['leftPagin'] = range(1, $iCount);
-        }
+            if ($this->aParams['active'] == 1) {
+                $this->aParams['leftButtonDisable'] = 1;
+            }
 
-        if ($this->aParams['active'] == 1) {
-            $this->aParams['leftButtonDisable'] = 1;
-        }
-
-        if ($this->aParams['active'] == $iCount) {
-            $this->aParams['rightButtonDisable'] = 1;
+            if ($this->aParams['active'] == $iCount) {
+                $this->aParams['rightButtonDisable'] = 1;
+            }
         }
 
         parent::initRender();
--- application/views/pagination.php
+++ application/views/pagination.php
@@ -18,50 +18,52 @@
 
 $sPath = $path;
 ?>
-<nav>
-    <ul class="pagination justify-content-end">
-        <li class="page-item <?if (!empty($leftButtonDisable)):?> disabled <?endif;?>">
-            <a class="page-link" href="<?=$sPath . ($active - 1)?>">
-                            <span aria-hidden="true">
-                                <i class="fa fa-angle-left"></i>
-                            </span>
-                <span class="sr-only">Назад</span>
-            </a>
-        </li>
-        <?if (!empty($leftPagin)): ?>
-            <?foreach ($leftPagin as $iItem):?>
-                <li class="page-item <?if($iItem == $active):?> active<?endif?>">
-                    <a class="page-link" href="<?=$sPath . $iItem?>"><?=$iItem?></a>
-                </li>
-            <?endforeach;?>
-        <?endif;?>
-        <?if (!empty($midlePagin)):?>
-            <li class="page-item disabled">
-                <a class="page-link" href="javascript:void(0)">...</a>
+<? if ($count > 1): ?>
+    <nav>
+        <ul class="pagination justify-content-end">
+            <li class="page-item <?if (!empty($leftButtonDisable)):?> disabled <?endif;?>">
+                <a class="page-link" href="<?=$sPath . ($active - 1)?>">
+                                <span aria-hidden="true">
+                                    <i class="fa fa-angle-left"></i>
+                                </span>
+                    <span class="sr-only">Назад</span>
+                </a>
             </li>
-            <?foreach ($midlePagin as $iItem):?>
-                <li class="page-item <?if($iItem == $active):?> active<?endif?>">
-                    <a class="page-link" href="<?=$sPath . $iItem?>"><?=$iItem?></a>
+            <?if (!empty($leftPagin)): ?>
+                <?foreach ($leftPagin as $iItem):?>
+                    <li class="page-item <?if($iItem == $active):?> active<?endif?>">
+                        <a class="page-link" href="<?=$sPath . $iItem?>"><?=$iItem?></a>
+                    </li>
+                <?endforeach;?>
+            <?endif;?>
+            <?if (!empty($midlePagin)):?>
+                <li class="page-item disabled">
+                    <a class="page-link" href="javascript:void(0)">...</a>
                 </li>
-            <?endforeach;?>
-        <?endif;?>
-        <?if (!empty($rightPagin)): ?>
-            <li class="page-item disabled">
-                <a class="page-link" href="javascript:void(0)">...</a>
+                <?foreach ($midlePagin as $iItem):?>
+                    <li class="page-item <?if($iItem == $active):?> active<?endif?>">
+                        <a class="page-link" href="<?=$sPath . $iItem?>"><?=$iItem?></a>
+                    </li>
+                <?endforeach;?>
+            <?endif;?>
+            <?if (!empty($rightPagin)): ?>
+                <li class="page-item disabled">
+                    <a class="page-link" href="javascript:void(0)">...</a>
+                </li>
+                <?foreach ($rightPagin as $iItem):?>
+                    <li class="page-item <?if($iItem == $active):?> active<?endif?>">
+                        <a class="page-link" href="<?=$sPath . $iItem?>"><?=$iItem?></a>
+                    </li>
+                <?endforeach;?>
+            <?endif;?>
+            <li class="page-item <?if (!empty($rightButtonDisable)):?> disabled <?endif;?>">
+                <a class="page-link" href="<?=$sPath . ($active + 1)?>">
+                                <span aria-hidden="true">
+                                    <i class="fa fa-angle-right"></i>
+                                </span>
+                    <span class="sr-only">Вперед</span>
+                </a>
             </li>
-            <?foreach ($rightPagin as $iItem):?>
-                <li class="page-item <?if($iItem == $active):?> active<?endif?>">
-                    <a class="page-link" href="<?=$sPath . $iItem?>"><?=$iItem?></a>
-                </li>
-            <?endforeach;?>
-        <?endif;?>
-        <li class="page-item <?if (!empty($rightButtonDisable)):?> disabled <?endif;?>">
-            <a class="page-link" href="<?=$sPath . ($active + 1)?>">
-                            <span aria-hidden="true">
-                                <i class="fa fa-angle-right"></i>
-                            </span>
-                <span class="sr-only">Вперед</span>
-            </a>
-        </li>
-    </ul>
-</nav>
+        </ul>
+    </nav>
+<? endif; ?>
