db2bcbdf1dcfcd51ca3af3796698bf6ffd3a15b0
max7722 <max.milash@yandex.ru>
2018-06-24 14:14:20 +0300

    Новая страница "О компании"

---
+++ application/controllers/About.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Max
+ * Date: 13.06.2018
+ * Time: 20:34
+ */
+
+namespace application\controllers;
+
+
+use application\core\PageController;
+
+class About extends PageController
+{
+    public function actionIndex()
+    {
+        $this->oContent->head->title = 'О компании';
+
+        $oAbout = new \application\core\view\About\About();
+        $this->oContent->content->addItem($oAbout);
+
+        $this->oContent->render();
+    }
+}
---
+++ application/views/about/about.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<div class="content">
+    <h2 class="content-heading">О Нас</h2>
+    <div class="block">
+        <div class="block-content">
+            <span><b>Компания</b> ООО "БытТех"</span><br>
+            <span><b>Адресс:</b> Смоленск, Россия, 214025 улица Нормандия-Неман, 31 </span>
+            <br>
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A5ca0522ea64def8a5dad98a5878d3037aa562a255c258d6776a7ea49924cf814&amp;width=754&amp;height=489&amp;lang=ru_RU&amp;scroll=true"></script>
+
+        </div>
+    </div>
+</div>
